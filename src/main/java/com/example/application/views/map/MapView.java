baef7c758ddb3dfdda00ed50a718d3c2c28aa33f
kylewinfield <s3541867@student.rmit.edu.au>
2021-11-19 18:44:24 +1100

    geolocation working based on IP

--- src/main/java/com/example/application/views/map/MapView.java
+++ src/main/java/com/example/application/views/map/MapView.java
@@ -1,11 +1,22 @@
 package com.example.application.views.map;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Page;
+import com.vaadin.flow.dom.Element;
+
 import org.vaadin.elmot.flow.sensors.GeoLocation;
 import org.vaadin.elmot.flow.sensors.Position;
 
 import com.example.application.components.leafletmap.LeafletMap;
 import com.vaadin.flow.router.Route;
+
+import io.ipgeolocation.api.Geolocation;
+import io.ipgeolocation.api.GeolocationParams;
+import io.ipgeolocation.api.IPGeolocationAPI;
+
 import com.vaadin.flow.router.PageTitle;
 import com.example.application.views.MainLayout;
 
@@ -13,6 +24,9 @@ import com.example.application.views.MainLayout;
 @Route(value = "map", layout = MainLayout.class)
 public class MapView extends VerticalLayout {
 
+	private static final int EARTH_RADIUS = 6371;
+	
+	
 	
     private LeafletMap map = new LeafletMap();
 
@@ -24,30 +38,30 @@ public class MapView extends VerticalLayout {
         map.setView(55.0, 10.0, 4);
         add(map);
         
-        geoLocater();
+        IPGeolocationAPI api = new IPGeolocationAPI("4960d2a06473426eb14c8863cc45e9b4");
+    	
+    	// Get geolocation for IP address (1.1.1.1) and fields (geo, time_zone and currency)
+    	   GeolocationParams geoParams = new GeolocationParams();
+    	   // need to create dynamic way of obtaining user's IP address 
+    	   geoParams.setIPAddress("2001:8004:1500:1144:b151:aebf:9cf:3a56");
+    	   geoParams.setFields("geo,time_zone,currency");
+
+    	  Geolocation geolocation = api.getGeolocation(geoParams);
+
+    	   // Check if geolocation lookup was successful
+    	    if(geolocation.getStatus() == 200) {
+    	       System.out.println(geolocation.getLongitude());
+    	       System.out.println(geolocation.getLatitude());
+    	   } else {
+    	       System.out.printf("Status Code: %d, Message: %s\n", geolocation.getStatus(), geolocation.getMessage());
+    	   }
         
-        //Position test = thisLocation.getValue();
-        //map.setView(geoLocation.getValue().getLongitude(), geoLocation.getValue().getLatitude(), 1);
-        
+        map.setView(Double.parseDouble(geolocation.getLatitude()), Double.parseDouble(geolocation.getLongitude())
+        		, 100);
     }
     
-    // Won't store valid coordinates, not sure if it is because it's not being deployed on real web server
-    public Position geoLocater() {
-    	
-    	 GeoLocation thisLocation = new GeoLocation();
-         thisLocation.setWatch(true);
-         thisLocation.setHighAccuracy(true);
-         thisLocation.setTimeout(100000);
-         thisLocation.setMaxAge(200000);
-         thisLocation.addValueChangeListener( e->{});
-         thisLocation.addErrorListener( e->{});
-         add(thisLocation);
-         
-        System.out.println("Location is: " + thisLocation);
-         
-        Position location = thisLocation.getValue();
-         
-		return location;
-    }
+    
+    
+   
  
 }
